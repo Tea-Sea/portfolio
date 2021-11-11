66e49772412bf41c07c07a704d341f779a97b294
Casparman <39361671+Casparman@users.noreply.github.com>
2021-11-11 23:42:18 +1100

    Node colour variety depending on attributes

--- src/Pages/Visualiser/Node/Node.jsx
+++ src/Pages/Visualiser/Node/Node.jsx
@@ -4,10 +4,30 @@ import { Component } from "react";
 import "./Node.css";
 
 export default class Node extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      node: this.determineColour(props),
+    };
+    this.determineColour(props);
+  }
+
+  determineColour(props) {
+    if (props.isStart) {
+      return "start";
+    }
+    if (props.isEnd) {
+      return "end";
+    }
+    console.log(props, "PROPS");
+    return "node";
+  }
+
   render() {
     const { column, row, isStart, isEnd } = this.props;
+
     return (
-      <div className="node">
+      <div className={this.state.node}>
         {column},{row}
       </div>
     );
