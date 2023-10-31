4a3bedfac08d141d670ee51086cbd19a6ce7c6b8
Casparman <39361671+Casparman@users.noreply.github.com>
2023-10-31 15:29:13 +1100

    Fixed 'randomiser' import

--- src/Pages/Visualiser/Algorithms/Maze_Generation/randomiser.jsx
+++ src/Pages/Visualiser/Algorithms/Maze_Generation/randomiser.jsx
@@ -1,15 +1,15 @@
 // Decides randomly whether a node is a wall or not
 // Mostly for testing purposes.
-export default function Randomise(grid, rows, columns) {
+export function randomise(grid, rows, columns) {
   const density = 0.25;
   for (var i = 0; i < rows; i++) {
     for (var j = 0; j < columns; j++) {
-      DetermineWall(grid[i][j], density);
+      determineWall(grid[i][j], density);
     }
   }
 }
 
-function DetermineWall(node, density) {
+function determineWall(node, density) {
   const rand = Math.random();
   if (rand < density && !(node.isStart || node.isEnd)) {
     node.isWall = true;
